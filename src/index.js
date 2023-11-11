aa2df02d03c3b49ca5f55ff7d232d958c2e77911
hsupinski <124569325+hsupinski@users.noreply.github.com>
2023-11-11 13:42:11 +0100

    simple candlestick chart and bar chart test
    Nie wiem czy wrzucić node_modules bo to waży ~300 mb jak trzeba to dorzucę

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const domContainer = document.querySelector('#app');
+ReactDOM.render(<App />, domContainer);
