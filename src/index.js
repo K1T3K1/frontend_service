afb4579dd9bd837a0da36f144f097219a2ceed28
hsupinski <124569325+hsupinski@users.noreply.github.com>
2023-12-01 14:56:40 +0100

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-const domContainer = document.querySelector('#app');
-ReactDOM.render(<App />, domContainer);
